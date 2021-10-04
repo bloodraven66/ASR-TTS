41217a7f93b9b829ea07be952e90204ab007e0c9
wtc6 <sathvikudupa66@gmail.com>
2021-10-04 13:27:28 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name='ASR_TTS',
+    version='0.0.3',
+    author='Mike Huls',
+    author_email='mike_huls@hotmail.com',
+    description='Testing installation of Package',
+    url='https://github.com/bloodraven66/ASR_TTS',
+    license='MIT',
+    packages=['ASR_TTS'],
+    install_requires=['requests'],
+)
