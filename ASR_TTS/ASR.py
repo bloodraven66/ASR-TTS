459a43e21e9769119816de2fc2a2546ae73c5d2f
wtc6 <sathvikudupa66@gmail.com>
2021-10-05 09:50:13 +0530

    libro wer

--- ASR_TTS/ASR.py
+++ ASR_TTS/ASR.py
@@ -1,4 +1,9 @@
 from speechbrain.pretrained import EncoderDecoderASR
+from utils import data_parse_libri, find_cer
+from tqdm import tqdm
+import torchaudio
+from torch.nn.utils.rnn import pad_sequence
+import torch
 
 class asr_model_info():
     def __init__(self):
@@ -19,7 +24,62 @@ ASR_MODEL_INFO  = asr_model_info()
 ASR_DATA_INFO = asr_data_info()
 
 
-def transcribe(path, modelname):
-    asr_model = EncoderDecoderASR.from_hparams(source=ASR_MODEL_INFO.model_dependencies[modelname][0],
-                                            savedir=ASR_MODEL_INFO.model_dependencies[modelname][1])
+def transcribe(path, modelname, use_cuda=False, return_obj=False):
+    if not use_cuda:
+        asr_model = EncoderDecoderASR.from_hparams(source=ASR_MODEL_INFO.model_dependencies[modelname][0],
+                                                savedir=ASR_MODEL_INFO.model_dependencies[modelname][1])
+
+    else:
+        asr_model = EncoderDecoderASR.from_hparams(source=ASR_MODEL_INFO.model_dependencies[modelname][0],
+                                                savedir=ASR_MODEL_INFO.model_dependencies[modelname][1],
+                                                run_opts={"device":"cuda"})
+    if return_obj:
+            return asr_model
     return asr_model.transcribe_file(path)
+
+def libri_transcribe(path, model, limit_sentence_count=None, batch_size=2, use_batch=True):
+    path = data_parse_libri(path)
+    cer = []
+    wer = []
+    audio_files, text = [], []
+    cers, wers = [], []
+    if use_batch:
+        asr_model = transcribe(None, model, True, True)
+    for key in tqdm(path):
+        if use_batch:
+            if len(audio_files)<batch_size:
+                audio_files.append(key)
+                text.append(path[key])
+                if len(cer)>0:
+                    if len(audio_files)+len(cer)>=limit_sentence_count:
+                        break
+            else:
+                sigs=[]
+                lens=[]
+                for audio_file in audio_files:
+                    snt, fs = torchaudio.load(audio_file)
+                    sigs.append(snt.squeeze())
+                    lens.append(snt.shape[1])
+                batch = pad_sequence(sigs, batch_first=True, padding_value=0.0)
+                lens = torch.Tensor(lens) / batch.shape[1]
+                preds = asr_model.transcribe_batch(batch, lens)[0]
+                for i in range(len(audio_files)):
+                    cer_val, wer_val, cer_, wer_ = find_cer(preds[i], text[i])
+                    cers.append(cer_val)
+                    wers.append(wer_val)
+                cer.extend(cers)
+                wer.extend(wers)
+
+                audio_files, text = [], []
+                cers, wers = [], []
+        else:
+            pred = transcribe(key, model, True)
+            cer_val, wer_val, cer_, wer_ = find_cer(pred, path[key])
+            cer.append(cer_val)
+            wer.append(wer_val)
+            if limit_sentence_count is not None:
+                if len(cer)>=limit_sentence_count:
+                    break
+
+    print(f'CER: {sum(cer)/len(cer)}, WER: {sum(wer)/len(wer)}')
+libri_transcribe('../../../other_tts_data/librispeech/test_clean/test_clean/', 'wav2vec', limit_sentence_count=5)
--- ASR_TTS/utils.py
+++ ASR_TTS/utils.py
@@ -1,11 +1,13 @@
-from IPython.display import HTML, Audio
-from google.colab.output import eval_js
-from base64 import b64decode
-import numpy as np
-from scipy.io.wavfile import read as wav_read
-import io
-import ffmpeg
-import soundfile as sf
+# from IPython.display import HTML, Audio
+# from google.colab.output import eval_js
+# from base64 import b64decode
+# import numpy as np
+# from scipy.io.wavfile import read as wav_read
+# import io
+# import ffmpeg
+# import soundfile as sf
+from pathlib import Path
+import re
 
 AUDIO_HTML = """
 <script>
@@ -114,3 +116,74 @@ def get_audio():
 def save_audio(y, sr, path):
     print(f'Audio saved at {path}...')
     sf.write(path, y, sr)
+
+def get_files(path, extension):
+    if isinstance(path, str): path = Path(path).expanduser().resolve()
+    return [str(n) for n in list(path.rglob(f'*{extension}'))]
+
+def data_parse_libri(path):
+    audio_files = get_files(path, '.flac')
+    files = get_files(path, '.txt')
+    text_map = {}
+    for filename in files:
+        with open(filename, 'r') as f:
+            data = f.read()
+        data = data.split('\n')
+        for line in data:
+            if len(line)>0:
+                textid = line.split('-')
+                text_map['-'.join(textid[:2])+'-'+textid[2].split(' ')[0]] = ' '.join(' '.join(textid[2:]).split(' ')[1:])
+    mapping = {}
+    for filename in audio_files:
+        id = filename.split('/')[-1].strip('.flac')
+        text = text_map[id]
+        mapping[filename] = text
+    return mapping
+
+import re
+
+def levenshtein(u, v):
+    prev = None
+    curr = [0] + list(range(1, len(v) + 1))
+    prev_ops = None
+    curr_ops = [(0, 0, i) for i in range(len(v) + 1)]
+    for x in range(1, len(u) + 1):
+        prev, curr = curr, [x] + ([None] * len(v))
+        prev_ops, curr_ops = curr_ops, [(0, x, 0)] + ([None] * len(v))
+        for y in range(1, len(v) + 1):
+            delcost = prev[y] + 1
+            addcost = curr[y - 1] + 1
+            subcost = prev[y - 1] + int(u[x - 1] != v[y - 1])
+            curr[y] = min(subcost, delcost, addcost)
+            if curr[y] == subcost:
+                (n_s, n_d, n_i) = prev_ops[y - 1]
+                curr_ops[y] = (n_s + int(u[x - 1] != v[y - 1]), n_d, n_i)
+            elif curr[y] == delcost:
+                (n_s, n_d, n_i) = prev_ops[y]
+                curr_ops[y] = (n_s, n_d + 1, n_i)
+            else:
+                (n_s, n_d, n_i) = curr_ops[y - 1]
+                curr_ops[y] = (n_s, n_d, n_i + 1)
+    return curr[len(v)], curr_ops[len(v)]
+
+def find_cer(sentence1, sentence2):
+    wer = {}
+    cer = {}
+    keys = ['sub', 'ins', 'del', 'n']
+    wer = {i: [] for i in keys}
+    cer = {i: [] for i in keys}
+    _, (s, i, d) = levenshtein(sentence1, sentence2)
+    cer['sub'] = s
+    cer['ins'] = i
+    cer['del'] = d
+    cer['n'] = len(sentence1)
+    _, (s, i, d) = levenshtein(sentence1.split(), sentence2.split())
+    wer['sub'] = s
+    wer['ins'] = i
+    wer['del'] = d
+    wer['n'] = len(sentence1.split())
+    cer_val = 100.0 * (cer['sub'] + cer['ins'] + cer['del']) / cer['n']
+    wer_val = 100.0 * (wer['sub'] + wer['ins'] + wer['del']) / wer['n']
+    return cer_val, wer_val, cer, wer
+
+# data_stats(path='../../../other_tts_data/librispeech/test_other/test_other/')
